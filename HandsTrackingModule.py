ede5634260f3c4185da3c38722e01de25cb9a39f
Dasol Kim <zoocanon92@gmail.com>
2022-12-23 11:44:09 +0900

    Add findPosition of handDetector

--- HandsTrackingModule.py
+++ HandsTrackingModule.py
@@ -16,16 +16,28 @@ class handDetector():
 		self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.min_detection_confidence, self.min_tracking_confidence)
 		self.mpDraw = mp.solutions.drawing_utils
 
-	def findHands(self, img, draw = True):
+	def findHands(self, img, draw=True):
 		imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-		results = self.hands.process(imgRGB)
-		# print(results.multi_hand_landmarks)
-		
-		if results.multi_hand_landmarks:
-			if draw:
-				for handLms in results.multi_hand_landmarks:
+		self.results = self.hands.process(imgRGB)		
+		if self.results.multi_hand_landmarks:
+			for handLms in self.results.multi_hand_landmarks:
+				if draw:
 					self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
 		return img
+
+	def findPosition(self, img, handNo=0, draw=True):
+		h, w, c = img.shape
+		lmList = []
+		if self.results.multi_hand_landmarks:
+			myHand = self.results.multi_hand_landmarks[handNo]
+			for id, lm in enumerate(myHand.landmark):
+				cx, cy = int(lm.x * w), int(lm.y * h)
+				lmList.append([id, cx, cy])
+				if draw:
+					cv2.circle(img, (cx, cy), 10, (255, 0, 255), cv2.FILLED)
+		
+		return lmList
 
 def main():
 	prevTime = 0
@@ -40,8 +52,10 @@ def main():
 
 	while cam.isOpened():
 		success, img = cam.read()
-
-		img = detector.findHands(img, True)
+		img = detector.findHands(img)
+		lmList = detector.findPosition(img)
+		if len(lmList) != 0:
+			print(lmList[4])
 
 		currTime = time.time()
 		fps = 1/(currTime - prevTime)
